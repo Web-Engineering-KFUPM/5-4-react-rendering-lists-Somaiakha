a2ac169bf0c2027d1fc22b5ca4d76eef3cecfdac
SomaiaKha <s202257000@kfupm.edu.sa>
2026-02-25 11:28:36 +0300

    TASK4

--- 5-4-react-rendering-lists/src/components/TaskItem.jsx
+++ 5-4-react-rendering-lists/src/components/TaskItem.jsx
@@ -5,17 +5,20 @@ export default function TaskItem({ task, onToggle, onDelete }) {
   return (
     <li className="task">
       <label className="taskMain">
-        {/* TODO (TASK 4): Checkbox calls onToggle(task.id) on change
-            Also make it reflect task.isDone using checked={task.isDone} */}
-        <input type="checkbox" />
-
-       <span className="title">{task.title}</span>
-
+        <input
+          type="checkbox"
+          checked={task.isDone}
+          onChange={() => onToggle(task.id)}
+        />
+        <span className="title">{task.title}</span>
         {!task.isDone && <DueBadge dueDate={task.dueDate} />}
       </label>
 
-      {/* TODO (TASK 4): Delete button calls onDelete(task.id) on click */}
-      <button className="ghost" aria-label="Delete task">
+      <button
+        className="ghost"
+        aria-label="Delete task"
+        onClick={() => onDelete(task.id)}
+      >
         ✕
       </button>
     </li>
